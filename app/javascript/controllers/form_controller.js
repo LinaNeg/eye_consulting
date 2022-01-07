2237ca531cd65eda2d2dd25caad5cd527ecdae66
Lina Negouai <lina.negouai@wanadoo.fr>
2022-01-07 14:57:52 -0400

    stimulus form_controller.js ok

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -2,6 +2,25 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   connect() {
-    console.log("Hello from your first Stimulus controller")
+    console.log("Hello from your first Stimulus controller");
+    console.log(this.answerTarget);
+
+  }
+
+  static targets = ['category', 'question', 'answer'];
+
+  displayCategoryForm(event) {
+    event.preventDefault();
+    this.categoryTarget.classList.remove('d-none');
+  }
+
+  displayQuestionForm(event) {
+    event.preventDefault();
+    this.questionTarget.classList.remove('d-none');
+  }
+
+  displayAnswerForm(event) {
+    event.preventDefault();
+    this.answerTarget.classList.remove('d-none');
   }
 }
