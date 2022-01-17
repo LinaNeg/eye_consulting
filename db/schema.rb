4146849d035eb8a23ab6e1a8b33faa14e47113dd
Lina NEGOUAI <lina.negouai@wanadoo.fr>
2022-01-17 14:02:01 -0400

    add type to answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_133851) do
+ActiveRecord::Schema.define(version: 2022_01_17_180031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2022_01_14_133851) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "selected"
+    t.string "type"
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
